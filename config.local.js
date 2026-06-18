3f51c7769e23af53eacdaead658ec6c62f4f26ef
hun030519-hub <hun030519@gmail.com>
2026-06-18 07:35:10 +0000

    2차 수정

---
+++ config.local.js
@@ -0,0 +1,5 @@
+// config.local.js 파일 내용
+window.BUBIMAP_SUPABASE_CONFIG = {
+  supabaseUrl: "https://lhgbenqlcmbvagpcpjja.supabase.co",
+  supabaseAnonKey: "sb_publishable_gKGhOGLFzrY4FmVDBIms4g_O0s9VErm" // 캡처 화면에서 복사하셨던 키
+};
